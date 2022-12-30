2d235ef00bdc9d2c59ef2a5075d30dbdce84b685
HAKAN KIRIK <103992207+hakan-kirik@users.noreply.github.com>
2022-12-30 10:25:42 +0300

    Change temperature sensor

--- IotProject/Thermistor.h
+++ IotProject/Thermistor.h
@@ -11,10 +11,10 @@ private:
   Thermistor();
 
   int thermistorPin = A3;  //thermistopPin configuration
-  int Vo;
+ /* int Vo;
   float R4 = 10000;
   float logR2, R2, tKelvin, tCelsius, tFahrenheit;
-  float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+  float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;*/
 };
 
 #endif
